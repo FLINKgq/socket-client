927d0588a77106d083fe56701f4d98662114724d
FLINKgq <146103429+FLINKgq@users.noreply.github.com>
2024-04-25 14:19:14 +0300

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,28 +2,38 @@ import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
+// Импорт стилей
 import styles from "../styles/Main.module.css";
 
+// Объект с полями формы
 const FIELDS = {
   NAME: "name",
   ROOM: "room",
 };
 
+// Определение компонента Main
 const Main = () => {
+  // Деструктуризация полей формы
   const { NAME, ROOM } = FIELDS;
 
+  // Состояние для значений полей формы
   const [values, setValues] = useState({ [NAME]: "", [ROOM]: "" });
 
+  // Обработчик изменения значения в полях формы
   const handleChange = ({ target: { value, name } }) => {
     setValues({ ...values, [name]: value });
   };
 
+  // Обработчик клика по кнопке
   const handleClick = (e) => {
+    // Проверка, все ли поля заполнены
     const isDisabled = Object.values(values).some((v) => !v);
 
+    // Если не все поля заполнены, предотвращаем действие по умолчанию
     if (isDisabled) e.preventDefault();
   };
 
+  // Возвращение JSX-разметки компонента Main
   return (
     <div className={styles.wrap}>
       <div className={styles.container}>
@@ -55,6 +65,7 @@ const Main = () => {
             />
           </div>
 
+          {/* Кнопка для перехода в комнату чата */}
           <Link
             className={styles.group}
             onClick={handleClick}
