f4ef85d002a6906328e715025dcb6bf3b39bac98
FLINKgq <146103429+FLINKgq@users.noreply.github.com>
2024-04-25 14:16:35 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
+// Импорт React из библиотеки react
 import React from "react";
+
+// Импорт компонента AppRoutes из файла AppRoutes.js
 import AppRoutes from "./AppRoutes";
 
+// Определение функционального компонента App
 const App = () => (
+  // Возвращение JSX элемента, содержащего корневой элемент приложения
   <div className="container">
+    {/* Вставка компонента AppRoutes */}
     <AppRoutes />
   </div>
 );
 
+// Экспорт компонента App для использования в других частях приложения
 export default App;
