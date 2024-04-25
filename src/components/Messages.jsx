0952be579e7b42ca0efcc884b8cdd30ea1a4897d
FLINKgq <146103429+FLINKgq@users.noreply.github.com>
2024-04-25 14:24:26 +0300

    Update Messages.jsx

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -2,14 +2,18 @@ import React from "react";
 
 import styles from "../styles/Messages.module.css";
 
+// Определение компонента Messages
 const Messages = ({ messages, name }) => {
+  // Возвращение JSX-разметки компонента Messages
   return (
     <div className={styles.messages}>
-      {messages.map(({ user, message }, i) => {
+      {messages.map(({ user, message }, i) => {/* Маппинг сообщений */
         const itsMe =
           user.name.trim().toLowerCase() === name.trim().toLowerCase();
+           // Установка класса стиля в зависимости от того, кто отправил сообщение
         const className = itsMe ? styles.me : styles.user;
 
+        // Возвращение JSX-разметки для каждого сообщения
         return (
           <div key={i} className={`${styles.message} ${className}`}>
             <span className={styles.user}>{user.name}</span>
