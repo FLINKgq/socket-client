3fdb3f2eb239e20ee2fd44caad4e54b489cc7a5e
FLINKgq <146103429+FLINKgq@users.noreply.github.com>
2024-04-25 18:13:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+// Импорт React из библиотеки react
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
 
+// Импорт стилей из файла ./styles/main.css
 import "./styles/main.css";
+
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
+
+// Рендеринг приложения, обернутого в Router для обработки маршрутов
 root.render(
   <Router>
     <App />
