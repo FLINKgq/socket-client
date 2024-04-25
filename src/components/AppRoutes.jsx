4d89e3c21d62403c42d7611a16d1a0b2b519b9d3
FLINKgq <146103429+FLINKgq@users.noreply.github.com>
2024-04-25 14:16:45 +0300

    Update AppRoutes.jsx

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,14 +1,24 @@
+// Импорт React из библиотеки react
 import React from "react";
+
+// Импорт компонентов Routes и Route из библиотеки react-router-dom
 import { Routes, Route } from "react-router-dom";
 
+// Импорт компонентов Main и Chat из соответствующих файлов
 import Main from "./Main";
 import Chat from "./Chat";
 
+// Определение компонента AppRoutes
 const AppRoutes = () => (
+  // Возвращение компонента Routes, который определяет маршрутизацию в приложении
   <Routes>
+    {/* Маршрут для главной страницы */}
     <Route path="/" element={<Main />} />
+    
+    {/* Маршрут для страницы чата */}
     <Route path="/chat" element={<Chat />} />
   </Routes>
 );
 
+// Экспорт компонента AppRoutes для использования в других частях приложения
 export default AppRoutes;
